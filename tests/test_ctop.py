38619a1d8dfe2ce42971a5c88a2d8ef77a1a08c1
garethcmurphy <garethcmurphy@gmail.com>
2017-01-03 23:54:29 +0100

    Add unit test riemann solver

--- tests/test_ctop.py
+++ tests/test_ctop.py
@@ -13,6 +13,15 @@ class TestBasic(unittest.TestCase):
         print (prim)
         npt.assert_allclose(prim, testprim, rtol=1e-5)
 
+    def test_riemann(self):
+        a = SimulationHydro.SimulationHydro()
+        left = [1, 0.0, 1.4]
+        right = left
+        testflux = [0, 0.56, 0]
+        flux = a.riemann_solver(left, right)
+
+        npt.assert_allclose(flux, testflux, rtol=1e-5)
+
 
 if __name__ == '__main__':
     unittest.main()
